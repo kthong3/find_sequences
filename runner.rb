5d9e58491e7107c2fd9677a1d93026b888990e1f
kthong3 <kthongsakounh@gmail.com>
2018-03-14 13:10:30 -0500

    update naming convention

--- runner.rb
+++ runner.rb
@@ -1,16 +1,16 @@
 def find_sequences(array)
   sequence = ""
   i = 0
-  beginning_array = find_beginning(array)
-  end_array = find_end(array)
+  beginning = find_beginning(array)
+  ending = find_ending(array)
 
-  while i < beginning_array.length
-    if i == beginning_array.length - 1
-      sequence += beginning_array[i].to_s + "-" + end_array[i].to_s
-    elsif beginning_array[i] == end_array[i]
-      sequence += beginning_array[i].to_s + ", "
+  while i < beginning.length
+    if i == beginning.length - 1
+      sequence += beginning[i].to_s + "-" + ending[i].to_s
+    elsif beginning[i] == ending[i]
+      sequence += beginning[i].to_s + ", "
     else
-      sequence += beginning_array[i].to_s + "-" + end_array[i].to_s + ", "
+      sequence += beginning[i].to_s + "-" + ending[i].to_s + ", "
     end
     i += 1
   end
@@ -18,11 +18,11 @@ def find_sequences(array)
 end
 
 def find_beginning(array)
-  beginning = array.select {|number| number if !array.include?(number.pred)}
+  array.select {|number| number if !array.include?(number.pred)}
 end
 
-def find_end(array)
-  ending = array.select {|number| number if !array.include?(number.next)}
+def find_ending(array)
+  array.select {|number| number if !array.include?(number.next)}
 end
 
 array = [1,2,3,6,7,8,12,18,19,20]
